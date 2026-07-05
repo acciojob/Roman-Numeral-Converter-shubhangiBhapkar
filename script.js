e77ee3edecab65f37c7b7f1cf12ffc3e37138279
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-05 13:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function convertToRoman(num) {
-    let value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    let symbol = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
     let result = "";
 
-    for (let i = 0; i < value.length; i++) {
-        while (num >= value[i]) {
-            result += symbol[i];
-            num -= value[i];
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            result += symbols[i];
+            num -= values[i];
         }
     }
 
     return result;
 }
 
-console.log(convertToRoman(36)); 
+console.log(convertToRoman(36));
+
+module.exports = convertToRoman;
